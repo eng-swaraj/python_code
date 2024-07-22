70940638deb404b3e75b9e027f8309a510216245
swaraj kumar <chatwithswaraj.sk@gmail.com>
2024-07-22 15:41:01 +0530

    update code

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+
+print("hello")
+# Print the following pattern (check the sample output):
+# *
+# **
+# ***
+# ****
+# *****
+#code
+print('*')
+print('**')
+print('***')
+print('****')
+print('*****')
+
